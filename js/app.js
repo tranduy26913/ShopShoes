b662b85674c7bcba2a2875ddd6d28fd6b4fee5f9
tranduy <tranbaoduy4@gmail.com>
2022-04-19 20:36:02 +0700

    fix scroll

--- js/app.js
+++ js/app.js
@@ -1,49 +1,4 @@
 
-const products = [
-    {
-        id: "1",
-        name: "AIR JORDAN 1 TRIPLE WHITE",
-        price: 2500000,
-        img1: "assets/img/AIR JORDAN 1 Triple White.jpg",
-        img2: "assets/img/AIR JORDAN 1 Triple White_2.jpg"
-    },
-    {
-        id: "2",
-        name: "AIR JORDAN 1 TRIPLE RED ",
-        price: 2500000,
-        img1: "assets/img/AIR JORDAN 1 Triple White.jpg",
-        img2: "assets/img/AIR JORDAN 1 Triple White_2.jpg"
-    },
-    {
-        id:"3",
-        name:"YEEZY BOOST 700 'WASH ORANGE'",
-        price:2300000,
-        img1:"https://bizweb.dktcdn.net/100/336/177/products/18-476439e5-2b31-47c1-a993-96bb5b80c360.jpg",
-        img2:"https://bizweb.dktcdn.net/100/336/177/products/19-04f3da06-ae47-40d5-832b-cc9b2e53405e.jpg"
-    },
-    {
-        id:"4",
-        name:"AIR JORDAN 1 LOW 'ROYAL TOE'",
-        price:1800000,
-        img2:"https://bizweb.dktcdn.net/100/336/177/products/7-752595d9-bc09-4046-bfdd-b97d0100576b.jpg",
-        img1:"https://bizweb.dktcdn.net/100/336/177/products/6-123beec2-f549-4086-a99f-06bc82c9e148.jpg"
-    },
-    {
-        id:"5",
-        name:"Dunk Low Bordeaux",
-        price:2500000,
-        img1:"https://bizweb.dktcdn.net/100/336/177/products/1-8271b70b-9204-4cd5-b5ee-d3edc34ef74c.jpg",
-        img2:"https://bizweb.dktcdn.net/100/336/177/products/2-f1d35fd4-1b5b-4112-af78-580ca9333999.jpg"
-    },
-    {
-        id:"6",
-        name:"AIR FORCE 1 '07 ESSENTIALS 'BLACK PAISLEY'",
-        price:1000000,
-        img1:"https://bizweb.dktcdn.net/100/336/177/products/37.jpg",
-        img2:"https://bizweb.dktcdn.net/100/336/177/products/37.jpg"
-    }
-]
-
 const cartHeader = document.getElementById("cart-header")
 const productSpecial = document.getElementById("product-special")
 
